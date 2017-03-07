eba138b4166004e85ef259c58875bb813556ff4f
Anton Datsenko <das.datsenko@gmail.com>
2017-03-07 19:59:44 +0200

    added NotFound.aspx

---
+++ Pages/NotFound.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Store.Master" AutoEventWireup="true" CodeBehind="NotFound.aspx.cs" 
+    Inherits="GameStore.Pages.NotFound" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContent" runat="server">
+    <h1>Ошибка 404 - страница не найдена</h1>
+    <p>Среда ASP.NET не нашла указанную страницу.</p>
+    <p><a href="<%= GetHomeLink() %>">Перейти на главную?</a></p>
+</asp:Content>
---
+++ Pages/NotFound.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Routing;
+
+namespace GameStore.Pages
+{
+    public partial class NotFound : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string GetHomeLink()
+        {
+            string path = RouteTable.Routes.GetVirtualPath(null, null).VirtualPath;
+            return path;
+        }
+    }
+}
